722a2f2962e84eb12d670f71db33d1fb33cc6d7f
bananateo <teodorkamchev@gmail.com>
2024-03-14 11:53:55 +0200

    more fish functions
    fish movement

--- constants.py
+++ constants.py
@@ -7,3 +7,7 @@ FPS = 60
 FISH_HEIGHT = 20
 FISH_WIDTH = 40
 FISH_VEL = 4
+
+fishes_right = []
+fishes_left = []
+
--- fish.py
+++ fish.py
@@ -7,17 +7,29 @@ fishes = []
 
 
 def fish_generator(num_fishes):
-    for _ in range(num_fishes):
-        fish_y = random.randint(HEIGHT, -HEIGHT//2)
+    fish_y = random.randint(HEIGHT, -HEIGHT//2)
 
-        side = random.randint(1, 2)
-        # 1 is left, 2 is right
-        if side == 1:
-            fish_x = random.randint(-100, 0)
-        else:
-            fish_x = random.randint(WIDTH, WIDTH+100)
+    side = random.randint(1, 2)
+    # 1 is left, 2 is right
+
+    if side == 1:
+        fish_x = random.randint(-100, 0)
         fish = pygame.Rect(fish_x, fish_y, FISH_WIDTH, FISH_HEIGHT)
+        fishes_left.append(fish)
 
-        fishes.append(fish)
+    else:
+        fish_x = random.randint(WIDTH, WIDTH+100)
+        fish = pygame.Rect(fish_x, fish_y, FISH_WIDTH, FISH_HEIGHT)
+        fishes_right.append(fish)
 
-    return fishes
+
+
+def fish_movement():
+    for fish in fishes_left:
+        fish.x += FISH_VEL
+        if fish.x > WIDTH:
+            fishes_left.remove(fish)
+    for fish in fishes_right:
+        fish.x -= FISH_VEL
+        if fish.x < 0:
+            fishes_right.remove(fish)
