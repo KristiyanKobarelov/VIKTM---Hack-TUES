b5d89627ea5b06e73c88e6bc943f9ab4f21070b8
bananateo <teodorkamchev@gmail.com>
2024-03-14 12:23:25 +0200

    changes to collision

--- collision.py
+++ collision.py
@@ -2,7 +2,13 @@ import pygame
 from fish import *
 from constants import *
 
-def detect_collision(player_rect, fish_rect):
-    if player_rect.colliderect(fish_rect):
-        pygame.quit()
-        exit()
+def detect_collision():
+    for fish in fishes_left:
+        if player_rect.colliderect(fish):
+            pygame.quit()
+            exit()
+    
+    for fish in fishes_right:
+        if player_rect.colliderect(fish):
+            pygame.quit()
+            exit()
