f9e3975d07aae84c979ccbc47cf7220cf9e22974
bananateo <teodorkamchev@gmail.com>
2024-03-14 12:33:54 +0200

    crashes when collision (KRIS GO NAPRAVI US MAFIA)

--- collision.py
+++ collision.py
@@ -5,11 +5,13 @@ from constants import *
 def detect_collision():
     for fish in fishes_left:
         if player_rect.colliderect(fish):
-            pygame.quit()
-            exit()
+            player_heatlh -= 20
+            if(player_health == 0):
+                exit()
     
     for fish in fishes_right:
         if player_rect.colliderect(fish):
-            pygame.quit()
-            exit()
+            player_heatlh -= 20
+            if(player_health == 0):
+                exit()
 
