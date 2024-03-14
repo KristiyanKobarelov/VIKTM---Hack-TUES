1f06238e6970b244885b8fe02486fcfde96f108a
bananateo <teodorkamchev@gmail.com>
2024-03-14 12:48:18 +0200

    test removal

--- collision.py
+++ collision.py
@@ -1,3 +1,4 @@
+import pygame
 from fish import *
 from constants import *
 
@@ -6,12 +7,14 @@ def detect_collision():
     for fish in fishes_left:
         if player_rect.colliderect(fish):
             player_heatlh -= 20
+            fishes_left.remove(fish)
             if(player_health == 0):
                 exit()
     
     for fish in fishes_right:
         if player_rect.colliderect(fish):
             player_heatlh -= 20
+            fishes_right.remove(fish)
             if(player_health == 0):
                 exit()
 
