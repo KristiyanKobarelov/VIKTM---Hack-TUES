32f7f76659301cacd562c873137cae1b4958dd30
bananateo <teodorkamchev@gmail.com>
2024-03-15 20:19:49 +0200

    unspecial fish now run away

--- unspecial_fish.py
+++ unspecial_fish.py
@@ -1,12 +1,32 @@
 from constants import *
-
+from specialfish import *
 
 def unspecial_fish_movement():
     for fish in unspecial_fish_left:
-        fish.x += FISH_VEL
-        if fish.x > WIDTH:
-            unspecial_fish_left.remove(fish)
+            if calculate_radius_special_fish(fish):
+                if fish.x > WIDTH / 2:
+                    fish.x += 7
+                else:
+                    fish.x -= 7
+
+                if fish.y > HEIGHT / 2:
+                    fish.y += 5
+                else:
+                    fish.y -= 5
+            fish.x += SPECIAL_FISH_VEL
+            if fish.x > WIDTH:
+                unspecial_fish_left.remove(fish)
     for fish in unspecial_fish_right:
-        fish.x -= FISH_VEL
+        if calculate_radius_special_fish(fish):
+            if fish.x > WIDTH / 2:
+                fish.x += 7
+            else:
+                fish.x -= 7
+
+            if fish.y > HEIGHT / 2:
+                fish.y += 5
+            else:
+                fish.y -= 5
+        fish.x += SPECIAL_FISH_VEL
         if fish.x < -100:
             unspecial_fish_right.remove(fish)
