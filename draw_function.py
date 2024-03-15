56f79083799176bbc6c7c6360c2cdaf783c0a498
MartinValentinov <159445059+MartinValentinov@users.noreply.github.com>
2024-03-15 11:03:22 +0200

    ribi

--- draw_function.py
+++ draw_function.py
@@ -1,4 +1,5 @@
 from fish import *
+from random import randint
 from hostile_fish import *
 
 player_surf.fill('green')
@@ -22,6 +23,30 @@ new_cursor = pygame.image.load('Cursor/noun-viewfinder-92699.png').convert_alpha
 new_cursor = pygame.transform.rotozoom(new_cursor, 0, 0.1)
 pygame.mouse.set_visible(False)
 
+medusa_1 = pygame.image.load('Medusa/jellyfish-large1.png').convert_alpha()
+medusa_1 = pygame.transform.rotozoom(medusa_1, 0, 2)
+medusa_2 = pygame.image.load('Medusa/jellyfish-large2.png').convert_alpha()
+medusa_2 = pygame.transform.rotozoom(medusa_2, 0, 2)
+medusa_3 = pygame.image.load('Medusa/jellyfish-large3.png').convert_alpha()
+medusa_3 = pygame.transform.rotozoom(medusa_3, 0, 2)
+medusa_4 = pygame.image.load('Medusa/jellyfish-large4.png').convert_alpha()
+medusa_4 = pygame.transform.rotozoom(medusa_4, 0, 2)
+
+medusa = [medusa_1, medusa_2, medusa_3, medusa_4]
+medusa_index = 0
+
+medusa_surf = medusa[medusa_index]
+
+
+def medusa_animations():
+    global medusa_surf, medusa_index
+
+    medusa_index += 0.05
+    if medusa_index >= len(medusa):
+        medusa_index = 0
+    medusa_surf = medusa[int(medusa_index)]
+    return medusa_surf
+
 
 def draw(window, player_health, temp_x=0):
     # Draw everything on screen
@@ -40,12 +65,10 @@ def draw(window, player_health, temp_x=0):
         window.blit(fish_surf, (fish.x, fish.y))
 
     for hostile_fish in hostile_fishes_left:
-        hostile_fish_surf = pygame.Surface((HOSTILE_WIDTH, HOSTILE_HEIGHT))
-        hostile_fish_surf.fill('red')
+        hostile_fish_surf = medusa_animations()
         window.blit(hostile_fish_surf, (hostile_fish.x, hostile_fish.y))
     for hostile_fish in hostile_fishes_right:
-        hostile_fish_surf = pygame.Surface((HOSTILE_WIDTH, HOSTILE_HEIGHT))
-        hostile_fish_surf.fill('red')
+        hostile_fish_surf = medusa_animations()
         window.blit(hostile_fish_surf, (hostile_fish.x, hostile_fish.y))
 
     for fish in special_fish_left:
